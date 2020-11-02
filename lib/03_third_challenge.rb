960557c96da27ac7adcf8fad46e96a9a0af4de12
Melissa F <melissa.faulner@gmail.com>
2020-11-02 04:59:52 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -15,9 +15,13 @@ def third_challenge
         :status => "alive"
         },
       :hero_friends => [
-        {
-        
-      }]
+        {:name => "Benvolio",
+        :age => "17",
+        :attitude => "worried"},
+        {:name => "Mercutio",
+        :age => "18",
+        :attitude => "hot-headed"}
+        ]
    }, 
    :capulet => {
       :patriarch => {
@@ -33,7 +37,14 @@ def third_challenge
         :age => "15",
         :status => "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {:name => "Steven",
+        :age => "30",
+        :attitude => "confused"},
+        {:name => "Nurse",
+        :age => "44",
+        :attitude => "worried"}
+        ]
    }
   }
 
